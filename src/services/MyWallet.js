c1c07778e3fae463ee4d3ac62a35b02f5c3902c4
israel henrique castro pontes <israelpontes@MacBook-Pro-de-israel.local>
2022-11-21 12:41:48 -0300

    fix(db.js)

--- src/services/MyWallet.js
+++ src/services/MyWallet.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://mywallet-api-iw3e.onrender.com';
+const BASE_URL = 'https://mywallet-api-iw3e.onrender.com'
 
 function creatHeaders(){
   const auth = JSON.parse(localStorage.getItem("myWallet"));
